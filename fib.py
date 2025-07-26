7f41ac686d6bb8e64d3fa2a9db657602620083f6
Adewole Conde <phatboislym@protonmail.com>
2025-07-26 12:15:53 +0100

    add Fibonacci sequence implementation using iteration and recursion

---
+++ fib.py
@@ -0,0 +1,19 @@
+def iteration(size):
+    num1, num2 = 0, 1
+    for i in range(2, size + 1):
+        num1, num2 = num2, num1 + num2
+
+    return num2
+
+
+def recursion(size):
+    if size <= 0:
+        return 0
+    if size == 1:
+        return 1
+
+    return recursion(size - 1) + recursion(size - 2)
+
+
+print(iteration(29))
+print(recursion(29))
